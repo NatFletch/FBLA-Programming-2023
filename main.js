ed6039d9ab062a19ccc8de7a3ad457e0e8d536f3
NatFletch <nef111605@yahoo.com>
2022-12-06 12:16:45 -0600

    adding a new way for communication

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 const { app, BrowserWindow } = require('electron')
+const cp = require("child_process");
+const util = require("util");
+const fs = require("fs");
+const path = require("path")
+const execFile = util.promisify(cp.execFile);
+
+function findPython() {
+  const possibilities = [
+    // In packaged app
+    
+    path.join(process.resourcesPath, "python", "python.exe"),
+    // In development
+    path.join(__dirname, "python", "python.exe"),
+  ];
+  for (const path of possibilities) {
+    if (fs.existsSync(path)) {
+      return path;
+    }
+  }
+  console.log("Could not find python3, checked", possibilities);
+  app.quit();
+}
 
 const createWindow = () => {
+    findPython()
     const win = new BrowserWindow({
         width: 800,
         height: 600
     })
     win.setMenu(null);
     win.loadFile('./application/index.html');
-
-}
+  }
 
 app.whenReady().then(() => {
     app.commandLine.appendSwitch('no-sandbox');
---
+++ scripts/login.py
@@ -0,0 +1,6 @@
+from classes.UserHelper import LoginManager
+
+LoginManager = LoginManager()
+
+if __name__ == "__main__":
+    LoginManager.login_user(username, password)
