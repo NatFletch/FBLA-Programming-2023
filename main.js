77c90736f57fd4f86ef0689289df87b37411192c
NatFletch <nathfletche@gmail.com>
2023-01-04 12:51:17 -0600

    restructured file system to make things easier. added pyscript to make using python 20x easier

--- main.js
+++ main.js
@@ -1,34 +1,11 @@
 const { app, BrowserWindow } = require('electron')
-const cp = require("child_process");
-const util = require("util");
-const fs = require("fs");
-const path = require("path")
-const execFileSync = util.promisify(cp.execFile);
-
-function findPython() {
-  const possibilities = [
-    // In packaged app
-    
-    path.join(process.resourcesPath, "python", "python.exe"),
-    // In development
-    path.join(__dirname, "python", "python.exe"),
-  ];
-  for (const path of possibilities) {
-    if (fs.existsSync(path)) {
-      return path;
-    }
-  }
-  console.log("Could not find python3, checked", possibilities);
-  app.quit();
-}
 
 const createWindow = () => {
-    findPython()
     const win = new BrowserWindow({
         width: 800,
         height: 600
     })
-    win.setMenu(null);
+    //win.setMenu(null);
     win.loadFile('./application/index.html');
   }
 
