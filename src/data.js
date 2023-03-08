7c8effef63a1387a5a43166a61d417fba73981de
nnnolan <pestanonolan@gmail.com>
2023-03-08 14:35:26 -0600

    src for react and store
    im not positive if this is the direciton i want to go, but this is a good place to start

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const prizes = [
+    {
+      id: 1,
+      title: "Smithville High School T-Shirt",
+      img: "images/prize1.jpg",
+      price: 10,
+      info: "Smithville High School T-Shirt",
+    //   inCart: false,
+    //   count: 0,
+    //   total: 0
+    },
+
+    {
+        id: 2,
+        title: "Snack Voucher",
+        img: "images/prize2.jpg",
+        price: 5,
+        info: "Snack Voucher",
+        //   inCart: false,
+        //   count: 0,
+        //   total: 0
+    },
+
+    {
+        id: 3,
+        title: "Smithville High School Pen",
+        img: "images/prize3.jpg",
+        price: 3,
+        info: "Smithville High School Pen",
+        //   inCart: false,
+        //   count: 0,
+        //   total: 0
+    }
+];
