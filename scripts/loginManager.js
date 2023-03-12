fa9d7ecb7d74df72693561567f1d757aebcda7b5
NatFletch <nef111605@yahoo.com>
2023-03-12 00:34:42 -0600

    bug fix and organisation

--- scripts/loginManager.js
+++ scripts/loginManager.js
@@ -31,6 +31,54 @@ function registerClick(){
     addUserToDatabase(name, fullname, password, 0, 0, bday, grade)
 }
 
+function onLoginClick(){
+    const db_client = require('../modules/db_client')
+    const dialogue = require('../modules/dialogue')
+    const cache = require('../modules/cache')
+    const $ = require('jquery')
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+    if(!username.trim().length || !password.trim().length){
+        return dialogue.alert('Please fill out both the username and password fields.', "info")
+    }
+
+    db_client.query("SELECT * FROM user_profiles WHERE Username=$1", [username], (err, res) => {
+        if(res.rows[0] === undefined){
+            return dialogue.alert("User does not exist!", "danger")
+        }
+        if(password == res.rows[0]["password"]){
+            cache.setItem("logged_in", username)
+            cache.setItem("isTeacher", res.rows[0]["isteacher"])
+            dialogue.alert('Success logging in!', 'success')
+            window.location.replace("../application/index.html")
+            return
+        } else {
+            return dialogue.alert('Incorrect username or password', 'danger')
+        }
+    })
+
+}
+
+document.getElementById("username").onkeydown = function(e){
+    if(e.keyCode == 13){
+        onLoginClick()
+    }
+}
+
+document.getElementById("password").onkeydown = function(e){
+    if(e.keyCode == 13){
+        onLoginClick()
+    }
+}
+
+function logout(){
+    const cache = require('../modules/cache')
+    cache.setItem("logged_in", "none")
+    cache.setItem('isTeacher', 0)
+    window.location.reload()
+}
+
 function addUserToDatabase(username, name_full, password, points, teacher, birthday, grade){
     const db_client = require('../modules/db_client')
     dialogue.alert('Successfully made account', 'success')
