1c0f128b03a3db8b76cc3e1ad0caba8c55d37d90
nnnolan <pestanonolan@gmail.com>
2023-03-15 09:01:37 -0500

    make it an actual function lmfao

--- scripts/store.js
+++ scripts/store.js
@@ -57,40 +57,54 @@ function clearCart() {
 }
 
 
-db_client.query("SELECT * FROM user_profiles WHERE Username = $1", [user], (err, res) => {
-  if (!err) {
-    const userPoints = res.rows[0].points;
-    const totalCost = cartItems.reduce((total, item) => total + item.price, 0);
 
-    if (userPoints < totalCost) {
-      window.alert("Insufficient points for checkout!");
-    } else {
-      const remainingPoints = userPoints - totalCost;
-      db_client.query("UPDATE user_profiles SET points = $1 WHERE Username = $2", [remainingPoints, user], (err, res) => {
-        if (err) {
-          throw err;
-        }
+function checkout() {
+  const db_client = require("../modules/db_client");
+  const cache = require("../modules/cache");
 
-        //add prizes to user's inventory
-        // im not sure if this works
-        db_client.query("INSERT INTO user_inventory (Username, ItemID) VALUES ($1, $2)", [user, cartItems], (err, res) => {
+  var user;
+  
+  if(cache.getItem("logged_in") == null || cache.getItem("logged_in") == "none"){
+    window.location.replace("./login.html")
+  } else {
+    user = cache.getItem("logged_in");
+  }
+    
+  db_client.query("SELECT * FROM user_profiles WHERE Username = $1", [user], (err, res) => {
+    if (!err) {
+      const userPoints = res.rows[0].points;
+      const totalCost = cartItems.reduce((total, item) => total + item.price, 0);
+
+      if (userPoints < totalCost) {
+        window.alert("Insufficient points for checkout!");
+      } else {
+        const remainingPoints = userPoints - totalCost;
+        db_client.query("UPDATE user_profiles SET points = $1 WHERE Username = $2", [remainingPoints, user], (err, res) => {
           if (err) {
             throw err;
           }
 
-          //clears cart
-          clearCart();
-          
+          //add prizes to user's inventory
+          // im not sure if this works
+          db_client.query("INSERT INTO user_inventory (Username, ItemID) VALUES ($1, $2)", [user, cartItems], (err, res) => {
+            if (err) {
+              throw err;
+            }
+
+            //clears cart
+            clearCart();
+            
+          });
+
+
+          window.alert("Checkout successful!");
         });
-
-
-        window.alert("Checkout successful!");
-      });
+      }
+    } else {
+      throw err;
     }
-  } else {
-    throw err;
-  }
-});
+  });
+}
 
 // test this !
 $(document).ready(function(){
