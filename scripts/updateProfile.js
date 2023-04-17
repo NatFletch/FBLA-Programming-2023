4efa0cb4a7b5f4cc7d44adce0d1dc673e48ffeb3
Nathan Fletcher <nef111605@yahoo.com>
2023-04-17 08:54:14 -0500

    slc day 2

--- scripts/updateProfile.js
+++ scripts/updateProfile.js
@@ -8,6 +8,7 @@ require('jquery')(document).ready(($) =>{
     //modules
     const db_client = require("../modules/db_client")
     const cache = require("../modules/cache")
+    const dialogue = require("../modules/dialogue")
     //elements
     const username = $("#username")
     const fullname = $("#fullname")
@@ -30,39 +31,83 @@ require('jquery')(document).ready(($) =>{
         window.location.replace("./login.html")
     }
 
-    db_client.query("SELECT * FROM user_profiles WHERE Username = $1", [user], (err, res) => {
-        if(!err){
-            var isTeacher = res.rows[0].isteacher
-            if(isTeacher == 0){
-                user_role = "Student"
-            } else if(isTeacher == 1){
-                user_role = "Teacher"
-            } else if(isTeacher == 2) {
-                user_role = "Admin"
-            }
-
-            db_client.query("SELECT * FROM user_inventory WHERE Username = $1", [user], (err, res) => {
-                if(res.rows[0] === undefined){
-                    items.html("No Items")
-                } else {
-                    console.log(res.rows[0].items)
-                    items.html("Items: " + res.rows[0].items)
+    if (/\s/.test(user)) {
+        //check if a name was searched for instead of a username
+        db_client.query("SELECT * FROM user_profiles WHERE FullName = $1", [user], (err, res) => {
+            if(!err && res.rowCount !== 0){
+                var isTeacher = res.rows[0].isteacher
+                if(isTeacher == 0){
+                    user_role = "Student"
+                } else if(isTeacher == 1){
+                    user_role = "Teacher"
+                } else if(isTeacher == 2) {
+                    user_role = "Admin"
                 }
-            })
-            username.html(user)
-            fullname.html("Name: " + res.rows[0].fullname)
-            grade.html("Grade: " + res.rows[0].grade)
-            birthday.html("Birthday: " + res.rows[0].birthday)
-            role.html("Role: " + user_role)
-            if(cache.getItem("isTeacher") > 0){
-                points.html("Points: " + res.rows[0].points + ' <input type="text" id="input-points"> <button class="btn btn-primary" onclick="changePoints()">Update Points</button>')
+    
+                db_client.query("SELECT * FROM user_inventory WHERE Username = $1", [user], (err, res) => {
+                    if(res.rows[0] === undefined){
+                        items.html("No Items")
+                    } else {
+                        console.log(res.rows[0].items)
+                        items.html("Items: " + res.rows[0].items)
+                    }
+                })
+                username.html(res.rows[0].username)
+                fullname.html("Name: " + res.rows[0].fullname)
+                grade.html("Grade: " + res.rows[0].grade)
+                birthday.html("Birthday: " + res.rows[0].birthday)
+                role.html("Role: " + user_role)
+                if(cache.getItem("isTeacher") > 0){
+                    points.html("Points: " + res.rows[0].points + ' <input type="text" id="input-points"> <button class="btn btn-primary" onclick="changePoints()">Update Points</button>')
+                } else {
+                    points.html("Points: " + res.rows[0].points)
+                }
+                
+                
+            } else if (res.rowCount == 0){
+                return dialogue.alert("No user found with that name.", "danger")
             } else {
-                points.html("Points: " + res.rows[0].points)
+                throw err
             }
-            
-            
-        } else {
-            throw err
-        }
-    })
+        })
+    } else {
+        db_client.query("SELECT * FROM user_profiles WHERE Username = $1", [user], (err, res) => {
+            if(!err && res.rowCount !== 0){
+                var isTeacher = res.rows[0].isteacher
+                if(isTeacher == 0){
+                    user_role = "Student"
+                } else if(isTeacher == 1){
+                    user_role = "Teacher"
+                } else if(isTeacher == 2) {
+                    user_role = "Admin"
+                }
+    
+                db_client.query("SELECT * FROM user_inventory WHERE Username = $1", [user], (err, res) => {
+                    if(res.rows[0] === undefined){
+                        items.html("No Items")
+                    } else {
+                        console.log(res.rows[0].items)
+                        items.html("Items: " + res.rows[0].items)
+                    }
+                })
+                username.html(res.rows[0].username)
+                fullname.html("Name: " + res.rows[0].fullname)
+                grade.html("Grade: " + res.rows[0].grade)
+                birthday.html("Birthday: " + res.rows[0].birthday)
+                role.html("Role: " + user_role)
+                if(cache.getItem("isTeacher") > 0){
+                    points.html("Points: " + res.rows[0].points + ' <input type="text" id="input-points"> <button class="btn btn-primary" onclick="changePoints()">Update Points</button>')
+                } else {
+                    points.html("Points: " + res.rows[0].points)
+                }
+                
+                
+            } else if (res.rowCount == 0){
+                return dialogue.alert("No user found with that name.", "danger")
+            } else {
+                throw err
+            }
+        })
+    }
+    
 })
